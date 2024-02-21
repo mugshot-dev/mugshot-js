e24e06afe83bbb3cdd800308f7fc85bd3665afbd
codenoid <14269809+codenoid@users.noreply.github.com>
2024-02-22 04:13:05 +0700

    Release 1.0.6: use ES6

--- dist/client.js
+++ dist/client.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,8 +7,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MugshotClient = void 0;
 class MugshotClient {
     constructor(apiKey, baseUrl = 'https://v1.mugshot.dev') {
         this.apiKey = apiKey;
@@ -121,4 +118,4 @@ class MugshotClient {
         });
     }
 }
-exports.MugshotClient = MugshotClient;
+export { MugshotClient };
